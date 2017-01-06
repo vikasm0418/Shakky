5c6f994e15526faa2fc0fc2c4bc14fcebb83c284
vikas meena <vikasmeena0418@gmail.com>
2017-01-06 07:56:39 +0530

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -2,9 +2,16 @@
 from django.conf.urls import url
 from . import views
 
+app_name ='music'
+
 urlpatterns = [
+	
 	#/music/
     url(r'^$',views.index,name='index'),
+    
     #/music/18/
     url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
+
+    #/music/<album_id>/favourite
+    url(r'^(?P<album_id>[0-9]+)/favourite/$',views.favourite,name='favourite'),
 ]
