c23c62931d7164e67679324d32faa561922fad62
vikas meena <vikasmeena0418@gmail.com>
2017-01-08 04:06:33 +0530

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -7,11 +7,8 @@ app_name ='music'
 urlpatterns = [
 	
 	#/music/
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.IndexView.as_view(),name='index'),
     
     #/music/18/
-    url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
-
-    #/music/<album_id>/favourite
-    url(r'^(?P<album_id>[0-9]+)/favourite/$',views.favourite,name='favourite'),
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
 ]
