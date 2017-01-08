0d3007833ce1388db973992a4a9df98377e51dd0
vikas meena <vikasmeena0418@gmail.com>
2017-01-09 05:24:15 +0530

    Update urls.py

--- music/urls.py
+++ music/urls.py
@@ -6,9 +6,22 @@ app_name ='music'
 
 urlpatterns = [
 	
+     url(r'^register/$',views.UserFormView.as_view(),name='register'),
+
 	#/music/
     url(r'^$',views.IndexView.as_view(),name='index'),
     
     #/music/18/
     url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
-]
+
+
+	#/music/album/add/
+    url(r'album/add/$',views.AlbumCreate.as_view(),name='album-add'),
+
+    #/music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$',views.AlbumUpdate.as_view(),name='album-update'),
+
+    #/music/album/2/delete
+    url(r'album/(?P<pk>[0-9]+)/delete/$',views.AlbumDelete.as_view(),name='album-delete')
+
+   ]
