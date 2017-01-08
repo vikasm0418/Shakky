5fad66decbd1a4dfdea2cf8bcf749cfda4926257
vikas meena <vikasmeena0418@gmail.com>
2017-01-09 05:24:46 +0530

    Update models.py

--- music/models.py
+++ music/models.py
@@ -1,19 +1,27 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 class Album(models.Model):
 	artist = models.CharField(max_length=250)
 	album_title = models.CharField(max_length=250)
 	genre = models.CharField(max_length=100)
-	logo = models.CharField(max_length=1000)
+	logo = models.FileField()
+
+	def get_absolute_url(self):
+		return reverse('music:detail',kwargs={'pk' :self.pk})
 
 	def __str__(self):
-		return self.album_title + ' - ' + self.artist
+		return	self.album_title + ' - ' + self.artist + ' - ' + self.genre
+
 
 class Song(models.Model):
 	album = models.ForeignKey(Album,on_delete=models.CASCADE)
 	file_type = models.CharField(max_length=10)
 	song_title = models.CharField(max_length=250)
 	is_favourite = models.BooleanField(default=False)
-	def __str__(self):
-		return self.song_title
 
+	def __str__(self):
+		return	self.song_title 
+
+		
